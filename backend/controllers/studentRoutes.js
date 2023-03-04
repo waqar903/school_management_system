a81ae46e9e712d6dca6e5042d73d10a223e8dc17
Waqar Khan <k.waqar903@gmail.com>
2023-03-04 23:04:23 +0500

    Seperate CRUD logics by adding Controller

---
+++ backend/controllers/studentRoutes.js
@@ -0,0 +1,44 @@
+import { v4 as uuidv4 } from 'uuid';
+
+
+let MOCK_STUDENTS = [
+    {id: 1, name: "John Doe", age: 25, rollno:"F23-1001"},
+    {id: 2, name: "Johnny David", age: 25, rollno:"F23-1002"},
+    {id: 3, name: "Johnas Doe", age: 25, rollno:"F23-1003"},
+];
+
+
+export const getStudents = (req, res) => {
+    res.send(MOCK_STUDENTS);
+}
+
+export const addStudent = (req, res) => {
+    const new_student = {
+        ...req.body,
+        id: uuidv4(),
+        rollno: "F23-1004"
+    };
+    MOCK_STUDENTS.push(new_student);
+    res.send(`${req.body.name} have been registered as a new student`);
+}
+
+export const getStudent = (req, res) => {
+    const id = req.params.id;
+    const student = MOCK_STUDENTS.find(s => +s.id === +id);
+    res.send(student);
+}
+
+export const updateStudent = (req, res) => {
+    const {id} = req.params;
+    const {name, age } = req.body;
+    let studentFound = MOCK_STUDENTS.find(s => +s.id === +id);
+    if(name) studentFound.name = name;
+    if(age) studentFound.age = age;
+    res.send('updated a students');
+}
+
+export const deleteStudent = (req, res) => {
+    const {id} = req.params;
+    MOCK_STUDENTS = MOCK_STUDENTS.filter(s => +s.id !== +id);
+    res.send('deleted a students');
+}
--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,51 +1,19 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+
+import { getStudents, addStudent, getStudent, updateStudent, deleteStudent } from '../controllers/studentRoutes.js';
+
 
 const routes = express.Router();
 
-let MOCK_STUDENTS = [
-    {id: 1, name: "John Doe", age: 25, rollno:"F23-1001"},
-    {id: 2, name: "Johnny David", age: 25, rollno:"F23-1002"},
-    {id: 3, name: "Johnas Doe", age: 25, rollno:"F23-1003"},
-];
+routes.get('/', getStudents);
 
+routes.post('/', addStudent);
 
+routes.get('/:id', getStudent);
 
-routes.get('/', (req, res) => {
-    const students = MOCK_STUDENTS;
-    res.send(students);
-});
+routes.put('/:id', updateStudent);
 
-routes.post('/', (req, res) => {
-    const new_student = {
-        ...req.body,
-        id: uuidv4(),
-        rollno: "F23-1004"
-    };
-    MOCK_STUDENTS.push(new_student);
-    res.send(`${req.body.name} have been registered as a new student`);
-});
-
-routes.get('/:id', (req, res) => {
-    const id = req.params.id;
-    const student = MOCK_STUDENTS.find(s => +s.id === +id);
-    res.send(student);
-});
-
-routes.put('/:id', (req, res) => {
-    const {id} = req.params;
-    const {name, age } = req.body;
-    let studentFound = MOCK_STUDENTS.find(s => +s.id === +id);
-    if(name) studentFound.name = name;
-    if(age) studentFound.age = age;
-    res.send('updated a students');
-});
-
-routes.delete('/:id', (req, res) => {
-    const {id} = req.params;
-    MOCK_STUDENTS = MOCK_STUDENTS.filter(s => +s.id !== +id);
-    res.send('deleted a students');
-});
+routes.delete('/:id', deleteStudent);
 
 
 export default routes;
