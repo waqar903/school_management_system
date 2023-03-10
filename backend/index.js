328f48d503838223b9db542c4cee72ef898ca094
Jackline Njoroge <bobbinnej@gmail.com>
2023-03-10 19:19:41 +0300

    Backend dependency installation and configuration. (#3)
    * backend installation and configuration, fix nodemon bug
    
    * backend dependecy installation and configuration

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,35 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-import studentRoutes from './routes/studentRoutes.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import studentRoutes from './routes/studentRoutes.js';
 
 
-const app = express();
-
+// configurations
 dotenv.config({path: './.env'});
-const PORT = process.env.PORT;
-
-app.use(bodyParser.json());
+const app = express();
+app.use(express.json);
+app.use(bodyParser.json({limit:"30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.use(cors());
 
 app.use('/api/students', studentRoutes);
-
 app.use(errorHandler);
 
 
-app.listen(PORT, () => {console.log(`Server is running on port: ${PORT}`)});
+
+
+// mongoose setup to be uncommented upon add of the mongo url & port inside the .env
+
+// const PORT = process.env.PORT || 6001;
+// mongoose.connect(process.env.MONGO_URL, {
+//     useUnifiedTopology:true,
+//     useNewUrlParser:true
+// }).then(()=>{
+//     app.listen(PORT, ()=>console.log(`Server is running on port: ${PORT}`));
+// }).catch((error)=>console.log(`${error} did not connect`));
+
+
+
